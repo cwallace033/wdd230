157e38c1f90d259820d1a9fe7f5400bd6e70d6b4
cwallace033 <cmwallce033@gmail.com>
2024-05-29 14:24:13 -0600

    changed wording on windchill

--- chamber/scripts/windspeed.js
+++ chamber/scripts/windspeed.js
@@ -9,4 +9,4 @@ const windspeed = parseInt(windspeedElement.textContent);
 const windchill = Math.round(35.74 + 0.6215 * temperature - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16));
 
 const windchillElement = document.getElementById('windchill');
-windchillElement.textContent = 'Wind Chill ' +windchill+ '°F';
+windchillElement.textContent = 'Feels like ' +windchill+ '°F with windchill';
