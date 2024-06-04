46569fb4e63936af99130b67c84241390c67e112
cwallace033 <cmwallce033@gmail.com>
2024-06-04 13:10:21 -0600

    added lazy loading

---
+++ lesson07/scripts/getdates.js
@@ -0,0 +1,11 @@
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let currentDate = `${day}-${month}-${year}`;
+
+document.getElementById("year").innerHTML = `${year}`;
+
+let modified = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = `Last Modified: ${modified}`;
