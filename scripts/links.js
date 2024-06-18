d7f10b3de69b19a2c07887a973e5b425e7933040
cwallace033 <cmwallce033@gmail.com>
2024-06-18 15:09:18 -0600

    fixed spelling error

--- scripts/links.js
+++ scripts/links.js
@@ -1,20 +1,29 @@
-const baseurl = "https://cwallace033.github.io/wdd230/";
+const baseURL = "https://cwallace033.github.io/wdd230/";
 
-const linksurl = baseurl + "data/links.json";
+const linksURL = baseURL + "data/links.json";
 
 const weeksContainer = document.querySelector('#weeks');
 
 async function getLinksData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    displayLinks(data.lessons);
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            if (data && data.lessons) {
+                displayLinks(data.lessons);
+            } else {
+                console.error('JSON data does not contain lessons property');
+            }
+        } else {
+            console.error('Network response was not ok:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Fetch operation failed:', error);
+    }
 }
 
-
-
 const displayLinks = (lessons) => {
-
-    lessons.foreach((lesson) => {
+    lessons.forEach((lesson) => {
         let lessonItem = document.createElement('section');
         let lessonTitle = document.createElement('h4');
         let linksList = document.createElement('ul');
@@ -22,11 +31,11 @@ const displayLinks = (lessons) => {
         lessonTitle.textContent = `Lesson ${lesson.lesson}`;
         lessonItem.appendChild(lessonTitle);
 
-        lesson.links.foreach((link) => {
+        lesson.links.forEach((link) => {
             let listItem = document.createElement('li');
             let anchor = document.createElement('a');
 
-            anchor.href = baseurl + link.url;
+            anchor.href = baseURL + link.url;
             anchor.textContent = link.title;
             listItem.appendChild(anchor);
             linksList.appendChild(listItem);
@@ -37,4 +46,4 @@ const displayLinks = (lessons) => {
     });
 }
 
-getLinksData(linksurl);
+getLinksData(linksURL);
