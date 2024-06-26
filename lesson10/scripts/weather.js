7102a1cd117efcc8778b769f2aa4f0f6e5e63b8f
cwallace033 <cmwallce033@gmail.com>
2024-06-26 07:05:40 -0600

    fixed js and css for weather

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,14 +2,14 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid={95269c37d13c4d1151344fcad05b488c}&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=95269c37d13c4d1151344fcad05b488c&units=imperial';
 
 async function apiFetch(){
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
--- scripts/weather.js
+++ scripts/weather.js
@@ -3,14 +3,14 @@ const weatherIcon = document.querySelector('#weather-icon');
 const weatherDesc = document.querySelector(`#weather-desc`);
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=41.83&lon=-111.83&appid={95269c37d13c4d1151344fcad05b488c}&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=41.83&lon=-111.83&appid=95269c37d13c4d1151344fcad05b488c&units=imperial';
 
 async function apiFetch(){
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -23,10 +23,9 @@ apiFetch();
 
 function displayResults (data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    weatherDesc.innerHTML = `${data.weather.description}`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
     let desc =  data.weather[0].description;
+    weatherDesc.innerHTML = `${desc}`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = `${desc}`;
 }
